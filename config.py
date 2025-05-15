ad9fc2a875e298635052b50a628ff4450878c2da
Binardino <langlois.robin@gmail.com>
2025-05-15 10:56:10 +0200

    add Dict to config

--- config.py
+++ config.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-FONT_PATH = os.getenv("FONT_PATH")
-INPUT_DIR = os.getenv("INPUT_DIR", "./input")
+FONT_PATH  = os.getenv("FONT_PATH")
+INPUT_DIR  = os.getenv("INPUT_DIR", "./input")
 OUTPUT_DIR = os.getenv("OUTPUT_DIR", "./output")
+DICT_PATH  = os.getenv("DICT_PATH")
+JAMDICT_DB = os.getenv("JAMDICT_DB")
 
 if not FONT_PATH:
     raise ValueError("FONT_PATH is not defined in the .env file")
